a2668a4473a62575d64c735a102bc315d1509187
Suraj <surajmitake2003@gmail.com>
2025-02-03 21:44:15 +0530

    resolve env issues on cloudinary.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 3000;
 
 app.post("/upload", upload.single("water"), async (req, res) => {
     console.log(req.file.path);
-    //const response = await uploadOnCloudinary(req.file.path);
+    const response = await uploadOnCloudinary(req.file.path);
     res.status(200).json({data: "success"});
 })
-
+ 
 app.listen(PORT, () => {
     console.log(`⚙️  Server is Running on http://localhost:${PORT} `);
 });
