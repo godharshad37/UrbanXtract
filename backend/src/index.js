1b088120b292cae0f28afd8cc987e2b822528e10
Suraj <surajmitake2003@gmail.com>
2025-02-03 12:46:46 +0530

    file uplaod setup

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import app from "./app.js";
+import { upload } from "./middleware/multer.middleware.js";
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.post("/upload", upload.single("avatar"), async (req, res) => {
+    console.log(req.file.path);
+    const response = await uploadOnCloudinary(req.file.path);
+    res.status(200).json({data: "success"});
+})
+
+app.listen(PORT, () => {
+    console.log(`⚙️  Server is Running on http://localhost:${PORT} `);
+});
