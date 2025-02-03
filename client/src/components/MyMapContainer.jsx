36680db0f161f94c54814d15a2d6596a0f811eb3
Suraj <surajmitake2003@gmail.com>
2025-02-03 13:32:15 +0530

    map edit feature

--- client/src/components/MyMapContainer.jsx
+++ client/src/components/MyMapContainer.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, FeatureGroup } from 'react-leaflet'
+import { EditControl } from "react-leaflet-draw"
+import "leaflet-draw/dist/leaflet.draw.css"
 
 export const MyMapContainer = () => {
+  const _onCreate = (e) => {
+    console.log(e);
+  }
+  
+  const _onEdit = (e) => {
+    console.log(e);
+  }
+
+  const _onDelete = (e) => {
+    console.log(e);
+  }
+
     return <MapContainer center={[28.7041, 77.1025]} zoom={13} style={{height: "300px", width: "80%"}}>
+
+      <FeatureGroup>
+        <EditControl position='topright' onCreated={_onCreate} onEdited={_onEdit} onDeleted={_onDelete}
+        draw={{
+          circle: false,
+          polyline: false,
+          polygon: true,
+          circlemarker: false, 
+          marker: false,
+          rectangle: {
+            shapeOptions: {
+              color: "blue", // Define color to ensure properties are set
+            },
+          }
+        }}/>
+      </FeatureGroup>
+
         <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
