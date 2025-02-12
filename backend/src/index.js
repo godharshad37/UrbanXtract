d645c1f8d81f8ced98059b29c240950d66fb3c02
Suraj <surajmitake2003@gmail.com>
2025-02-12 11:59:49 +0530

    multer path change

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,11 +8,11 @@ dotenv.config({
 
 const PORT = process.env.PORT || 3000;
 
-// app.post("/upload", upload.single("sat"), async (req, res) => {
-//     console.log(req.file.path);
-//     const response = await uploadOnCloudinary(req.file.path);
-//     res.status(200).json({data: "success"});
-// })
+app.post("/upload", upload.single("sat"), async (req, res) => {
+    console.log(req.file);
+    //const response = await uploadOnCloudinary(req.file.path);
+    res.status(200).json({data: "success"});
+})
  
 app.listen(PORT, () => {
     console.log(`⚙️  Server is Running on http://localhost:${PORT} `);
--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './public/Image_Data');
+        cb(null, './public/Input');
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
